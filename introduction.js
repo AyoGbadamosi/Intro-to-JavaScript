311abd24d1f0e816c745ed0efc1ef571d82fa590
AyoGbadamosi <gbadamosiayo16@gmail.com>
2022-06-25 17:04:51 +0100

    intro to Javascript

--- introduction.js
+++ introduction.js
@@ -1,4 +1,6 @@
 // Write a javascript code that prints out your name, height, and country on the screen
-console.log("Gbadamosi Ayomide .I");
-console.log("Height: 178cm");
-console.log("Country: Nigeria");
+document.write("Name: Gbadamosi Ayomide .I");
+document.write("<br>")
+document.write("Height: 178cm");
+document.write("<br>")
+document.write("Country: Nigeria");
